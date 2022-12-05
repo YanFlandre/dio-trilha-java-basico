8fc6c029316075bb3a2fcee807b3a4b902d6a3ec
YanFlandre <jp_felippe@hotmail.com>
2022-12-05 15:40:54 -0300

    Primeiro Update

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,11 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        //TODO Conhecer e importar a classe scanner
+
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pelo scanner os valores digitados pelo usuário
+
+        //Exibir a mensagem conta criada
+    }
+}
